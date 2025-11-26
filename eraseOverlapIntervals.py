de3f9b1c813ffd82cfae2d402d45c23cb3769876
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-26 07:09:55 -0600

    Update eraseOverlapIntervals.py

--- eraseOverlapIntervals.py
+++ eraseOverlapIntervals.py
@@ -5,3 +5,7 @@ from typing import List
 
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+
+        count = 0
+        lastEnd = intervals[0][1]
