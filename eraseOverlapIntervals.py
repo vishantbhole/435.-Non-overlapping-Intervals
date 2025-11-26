ec4ee98ca18931b5b0be82758d193819597a5fc0
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-11-26 07:11:38 -0600

    Update eraseOverlapIntervals.py

--- eraseOverlapIntervals.py
+++ eraseOverlapIntervals.py
@@ -22,3 +22,6 @@ if __name__ == "__main__":
     sol = Solution()
     intervals = [[1,2],[2,3],[3,4],[1,3]]
     print("Output is : ", sol.eraseOverlapIntervals(intervals))
+
+    intervals2 = [[1,2],[1,2],[1,2]]
+    print("Output is : ", sol.eraseOverlapIntervals(intervals2))
